8fab73adfbe32c3fd8d9785eacd0eb2dcce3da8a
zmaseluta <dianabeleiu@gmail.com>
2014-04-03 19:40:35 +0300

    edit profile changes

--- V3Learning/src/main/java/ViewModel/MyProfileVM.java
+++ V3Learning/src/main/java/ViewModel/MyProfileVM.java
@@ -2,13 +2,16 @@ package ViewModel;
 
 import javax.xml.ws.Response;
 
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Sessions;
 
+import Model.DBOperations;
 import Model.User;
 
 public class MyProfileVM {
@@ -27,5 +30,10 @@ public class MyProfileVM {
 		setUser((User) Sessions.getCurrent().getAttribute("user"));
 		if(user == null)
 			Executions.sendRedirect("index.zul");
+	}
+	@Command
+	@NotifyChange("user")
+	public void saveChanges(){
+		
 	}
 }
