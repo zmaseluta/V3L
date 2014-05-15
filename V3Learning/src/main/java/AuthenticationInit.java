a2ccf312ad131d16bd1a7843c8efc56bb5125a5f
zmaseluta <dianabeleiu@gmail.com>
2014-05-15 16:09:20 +0300

    update

---
+++ V3Learning/src/main/java/AuthenticationInit.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.util.Initiator;
+
+import Model.User;
+
+public class AuthenticationInit implements Initiator {
+
+	public void doInit(Page page, Map<String, Object> args) throws Exception {
+
+		User usera = (User) Sessions.getCurrent().getAttribute("user");
+		if (usera == null) {
+			Executions.sendRedirect("login.zul");
+		}
+	}
+}
