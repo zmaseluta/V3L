37e353dae74ee2ae7cb965063ef38eaa5f2d840e
conflux92 <bodo_92@yahoo.com>
2014-06-13 17:07:50 +0300

    Download

---
+++ V3Learning/src/main/java/Model/DownloadFile.java
@@ -0,0 +1,73 @@
+package Model;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+ 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+
+public class DownloadFile {
+	public static void main(String[] args) {
+        String server = "v3l.host22.com";
+        int port = 21;
+        String user = "a1423157";
+        String pass = "echipab6";
+ 
+        FTPClient ftpClient = new FTPClient();
+        try {
+ 
+            ftpClient.connect(server, port);
+            ftpClient.login(user, pass);
+            ftpClient.enterLocalPassiveMode();
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+ 
+            // APPROACH #1: using retrieveFile(String, OutputStream)
+            String remoteFile1 = "public_html/test/295230_347475008670268_236976138_n.jpg";
+            File downloadFile1 = new File("D:/Downl/295230_347475008670268_236976138_n.jpg");
+            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+            outputStream1.close();
+ 
+            if (success) {
+                System.out.println("File #1 has been downloaded successfully.");
+            }
+ 
+            // APPROACH #2: using InputStream retrieveFileStream(String)
+            String remoteFile2 = "public_html/test/Report.doc";
+            File downloadFile2 = new File("D:/Downl/Report.doc");
+            OutputStream outputStream2 = new BufferedOutputStream(new FileOutputStream(downloadFile2));
+            InputStream inputStream = ftpClient.retrieveFileStream(remoteFile2);
+            byte[] bytesArray = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(bytesArray)) != -1) {
+                outputStream2.write(bytesArray, 0, bytesRead);
+            }
+ 
+            success = ftpClient.completePendingCommand();
+            if (success) {
+                System.out.println("File #2 has been downloaded successfully.");
+            }
+            outputStream2.close();
+            inputStream.close();
+ 
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
